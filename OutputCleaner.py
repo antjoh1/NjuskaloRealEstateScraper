7de9ed94f0e971324244755d242e3f218d41ad33
antjoh1 <johman.antonio@gmail.com>
2024-10-12 18:25:22 +0200

    test

--- OutputCleaner.py
+++ OutputCleaner.py
@@ -3,6 +3,13 @@ import sqlite3
 
 class OutputCleaner():
 
+    def __init__(self):
+        self.con = sqlite3.connect("scrape-output/test.db")
+        self.cur = self.con.cursor()
+
+    def _setupTable (self): 
+        self.cur.execute("CREATE TABLE movie(title, year, score)")
+
     def cleanDuplicates(self, json_input):
         # Remove duplicate listings if any 
         unique_json = { each['id']:  each for each in json_input }.values()
@@ -13,10 +20,12 @@ class OutputCleaner():
 if __name__ == '__main__':
     file_name = 'scrape-output/prodaja-stanovazagreb.json'
 
-    with open(file_name) as f:
-        original = json.load(f)
+    # with open(file_name) as f:
+    #     original = json.load(f)
 
     a = OutputCleaner()
-    test = a.cleanDuplicates(file_name)
-
-    print ("hello world")
+    # test = a.cleanDuplicates(file_name)
+    # a._setupTable()
+    res = a.cur.execute("SELECT name FROM sqlite_master")
+    print( res.fetchone())
+    # print ("hello world")
